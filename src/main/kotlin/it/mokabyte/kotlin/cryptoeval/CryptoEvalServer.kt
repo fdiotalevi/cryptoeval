609d7f2a0b6eeab005ad04733ab6e2fc862e9e42
Filippo Diotalevi <filippo.diotalevi@johnlewis.co.uk>
2018-02-10 11:09:30 +0000

    1. client

--- src/main/kotlin/it/mokabyte/kotlin/cryptoeval/CryptoEvalServer.kt
+++ src/main/kotlin/it/mokabyte/kotlin/cryptoeval/CryptoEvalServer.kt
@@ -4,12 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import org.http4k.client.ApacheClient
 import org.http4k.core.Method
 import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status.Companion.OK
-import org.http4k.core.queries
 import org.http4k.format.Jackson.parse
-import org.http4k.server.Jetty
-import org.http4k.server.asServer
 import java.math.BigDecimal
 
 
@@ -22,29 +17,16 @@ fun retrieveQuotes() : List<Quote> {
     val request = Request(Method.GET, "https://api.coinmarketcap.com/v1/ticker/?limit=20")
     val jsonResponse = parse(client(request).bodyString())
 
-    fun toQuote(jsonQuote: JsonNode) = Quote(symbol = jsonQuote["symbol"].asText(), priceUsd = jsonQuote["price_usd"].asText().toBigDecimal())
+    fun toQuote(jsonQuote: JsonNode) = Quote(
+            symbol = jsonQuote["symbol"].asText(),
+            priceUsd = jsonQuote["price_usd"].asText().toBigDecimal()
+    )
+
     return jsonResponse.map(::toQuote)
 }
 
 
-fun List<Quote>.findQuote(symbol: String) = this.find { it.symbol.toUpperCase() == symbol.toUpperCase() }?.priceUsd ?: BigDecimal.ZERO
-
-
-fun endpoint(request: Request) : Response {
-
-    val quotes = retrieveQuotes()
-
-    val portfolio = request.uri
-            .queries()
-            .map { ( symbol, amount) -> BigDecimal(amount) * quotes.findQuote(symbol) }
-            .fold(BigDecimal.ZERO, { first, second ->  first + second })
-
-
-    return Response(OK).body("$portfolio")
-}
-
-
 fun main(args: Array<String>) {
-    ::endpoint.asServer(Jetty(9000)).start()
+    println(retrieveQuotes())
 }
 
